5ececd93e8fcf15d1cbf3654cdfd8c914fe334c2
Felipe Vieira <felipe.svfx@gmail.com>
2021-04-10 17:51:40 -0300

    chore: init store module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { BackofficeModule } from 'src/modules/backoffice/api/backoffice.module';
+import { StoreModule } from 'src/modules/store/store.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/petshop'), BackofficeModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/petshop'), BackofficeModule, StoreModule],
   controllers: [],
 })
 export class AppModule {}

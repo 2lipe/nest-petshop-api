91f92a0080ee1879ecd571769ddde8e8c4d0b74e
Felipe Vieira <felipe.svfx@gmail.com>
2021-04-09 23:14:38 -0300

    chore: connect with mongo using Docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { BackofficeModule } from './backoffice/backoffice.module';
 
 @Module({
-  imports: [BackofficeModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/petshop'),
+    BackofficeModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/backoffice/backoffice.module.ts
+++ src/backoffice/backoffice.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { CustomerController } from './controllers/customer/customer.controller';
+import { CustomerSchema } from './schemas/customer.schema';
+import { UserSchema } from './schemas/user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Customer',
+        schema: CustomerSchema,
+      },
+      {
+        name: 'User',
+        schema: UserSchema,
+      },
+    ]),
+  ],
   controllers: [CustomerController],
 })
 export class BackofficeModule {}

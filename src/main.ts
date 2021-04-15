e587a826f49436fbd0ada7dbfbe9a7eb88ba47a1
Felipe Vieira <felipe.svfx@gmail.com>
2021-04-15 16:50:09 -0300

    chore: create swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import * as compression from 'compression';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'src/app.module';
 import { CustomLogger } from 'src/shared/loggers/custom-logger.service';
@@ -11,6 +12,17 @@ async function bootstrap() {
 
   app.use(compression());
   app.setGlobalPrefix('api');
+
+  // Open API (Swagger)
+  const options = new DocumentBuilder()
+    .setTitle('Petshop API')
+    .setDescription('API do projeto petshop')
+    .setVersion('1.0.0')
+    .addTag('petshop')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT);
 }

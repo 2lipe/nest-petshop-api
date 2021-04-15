35b85b955afedfdbc56973765f8b65ca5e378993
Felipe Vieira <felipe.svfx@gmail.com>
2021-04-15 15:24:29 -0300

    chore: create compression

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import * as compression from 'compression';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'src/app.module';
 import { CustomLogger } from 'src/shared/loggers/custom-logger.service';
@@ -8,6 +9,7 @@ async function bootstrap() {
     logger: new CustomLogger(),
   });
 
+  app.use(compression());
   app.setGlobalPrefix('api');
 
   await app.listen(process.env.PORT);
